638863718e38c15037c5bf30eb6c21fff77b9ec8
Jake Wood <jakealistairwood@gmail.com>
2021-02-19 12:14:12 +0000

    passport and user schema in place

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-// const sass = require('node-sass-middleware');
+const session = require('express-session');
+const flash = require('connect-flash');
 const methodOverride = require('method-override');
 const Product = require('./models/product');
-const Review = require('./models/review');
 
 const products = require('./routes/products');
 const reviews = require('./routes/reviews');
@@ -30,17 +30,29 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
-// app.use(
-//     sass({
-//         src: __dirname + '/public',
-//         dest: __dirname + '/public/css',
-//         indentedSyntax: false,
-//         debug: true,
-//         sourceMap: true
-//     })
-// )
 app.use(express.static(path.join(__dirname + "/public")));
 app.use(methodOverride('_method'));
+
+// Set up session cookie
+const sessionConfig = {
+    secret: 'thisisasecret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7, // Milliseconds in a week
+        maxAge: 1000 * 60 * 60 * 24 * 7
+    }
+}
+app.use(session(sessionConfig));
+// app.use(flash());
+
+// app.use((req, res, next) => {
+//     res.locals.success = req.flash('success');
+//     res.locals.error = req.flash('error');
+//     next();
+// })
+
 app.use('/products', products);
 app.use('/products/:id/reviews', reviews);
 
@@ -50,14 +62,12 @@ app.get('/', async (req, res) => {
     res.render('home', { products });
 })
 
-
-
 app.post('/', async (req, res) => {
     const product = new Product(req.body.product);
     await product.save();
+    req.flash('success', 'You have successfully created a new product.');
     res.redirect(`/products/${product._id}`);
 })
-
 
 app.listen(3000, () => {
     console.log("Listening on Port 3000");
