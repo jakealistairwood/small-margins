706ed6841304f234c671d7589d06eef586537562
Jake Wood <jakealistairwood@gmail.com>
2021-02-23 12:51:48 +0000

    added stylings to flash modal

--- public/js/flash.js
+++ public/js/flash.js
@@ -1,5 +1,6 @@
 const flashModal = document.querySelector('.flash-modal');
 const closeFlashBtn = document.querySelector('.close-flash');
+const overlay = document.querySelector('.flash-overlay');
 
 const closeFlash = () => {
     flashModal.classList.add('hidden');
