88f46bd8b27c25ae836ca44d14753f1fa0d2d168
Jake Wood <jakealistairwood@gmail.com>
2021-02-24 19:22:10 +0000

    img src fix

--- public/js/info.js
+++ public/js/info.js
@@ -3,7 +3,7 @@
 const fixImgSrc = () => {
     const imgSrc = document.querySelector('.product-img').src;
     let newSrc = imgSrc.split('');
-    newSrc.splice(0, 22);
+    newSrc.splice(0, 41);
     newSrc = newSrc.join('');
     document.querySelector('.product-img').src = newSrc;
 }
